79276659d9528f130026df5e04f69684a31c2731
Abhinav-22 <abhinavanil9@gmail.com>
2022-06-18 12:56:46 +0530

    added text-to-audio

---
+++ texttospeech.py
@@ -0,0 +1,11 @@
+from gtts import gTTS
+
+def makeMP3(words, mp3name, language="en"):
+    tts = gTTS(text = words, lang = language)
+    tts.save("%s.mp3" % mp3name)
+    print("File %s.mp3 created" % mp3name)
+
+f = open("input.txt", 'r')
+text = f.read()
+
+makeMP3(text, "english")
