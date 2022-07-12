32a09113ba38caf773475cca542e4fb759ee3fab
Abhinav-22 <65677700+Abhinav-22@users.noreply.github.com>
2022-07-12 23:10:58 +0530

    Create Translator.py

---
+++ Translator.py
@@ -0,0 +1,11 @@
+from googletrans import Translator
+f = open("t.txt", "r")
+fin = f.read()
+trans = Translator()
+res = ""
+a = fin.split(".")
+for i in a :
+    a = 'hi'
+    out = trans.translate(i,  dest = a)
+    res = res + str(out.text)
+print(res)
